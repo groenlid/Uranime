ddbb2d684de27d78e18f33d5576a09ffd56bb66f
Mats Groenlid <groenlid@gmail.com>
2012-06-27 01:55:49 +0200

    some styling for special episodes

--- app/Console/Command/ScrapeShell.php
+++ app/Console/Command/ScrapeShell.php
@@ -695,21 +695,21 @@ class ScrapeShell extends AppShell {
 		$seasons = null; 	// null == all seasons
 
 		// Find out which seasons we should fetch
-
-		foreach(explode(",",$seasonsInfo) as $singleSlice) 
-		{	
-			// Singleslice could either be "1-3" or single "3" 
-			$rubbleSlice = explode("-",$singleSlice);
-			if(count($rubbleSlice) == 1) // if string is "3"; array("3") is returned
-			{
-				if(!in_array((int)trim($rubbleSlice[0]),$seasons,true))
-					$seasons[] = (int)trim($rubbleSlice[0]);
-				continue;
+		if($seasonsInfo != null)
+			foreach(explode(",",$seasonsInfo) as $singleSlice) 
+			{	
+				// Singleslice could either be "1-3" or single "3" 
+				$rubbleSlice = explode("-",$singleSlice);
+				if(count($rubbleSlice) == 1) // if string is "3"; array("3") is returned
+				{
+					if(!in_array((int)trim($rubbleSlice[0]),$seasons,true))
+						$seasons[] = (int)trim($rubbleSlice[0]);
+					continue;
+				}
+				for($i = (int)trim($rubbleSlice[0]); $i <= (int)trim($rubbleSlice[1]);$i++)
+					if(!in_array($i,$seasons))
+						$seasons[] = $i;
 			}
-			for($i = (int)trim($rubbleSlice[0]); $i <= (int)trim($rubbleSlice[1]);$i++)
-				if(!in_array($i,$seasons))
-					$seasons[] = $i;
-		}
 		
 		// Printing some information
 		if($seasons == null)
