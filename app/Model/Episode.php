74c8df203faa75d6497683b039562a1b5e94db4c
Mats Groenlid <groenlid@gmail.com>
2012-06-28 01:18:35 +0200

    moved episode-image function to model

--- app/Model/Episode.php
+++ app/Model/Episode.php
@@ -5,5 +5,31 @@ class Episode extends AppModel {
 	public $belongsTo = array('Anime');
 	public $hasMany = array('UserEpisode');
 	public $order = array('aired','number');
+
+
+	/**
+	 * Returns the thumbnail of the episode image
+	 * If $widht is 0, the full-size image is returned
+	 */
+	public function fetchImage($episode_id, $width = 0)
+	{
+		$episode = $this->find('first',array(
+			'conditions' => array(
+				'id' => $episode_id
+				)
+			)
+		);
+
+		$returnUrl = "";
+
+		if($width != 0 && is_numeric($width) && $width > 0)
+			$returnUrl .= "http://src.sencha.io/" . $width . "/";
+
+		if(file_exists(WWW_ROOT . EPISODE_IMAGE_PATH . $episode['Episode']['anime_id'] . "/" . $episode['Episode']['image']) && $episode['Episode']['image'] != null)
+			return $returnUrl . SERVER_PATH . EPISODE_IMAGE_PATH . $episode['Episode']['anime_id'] . "/" . $episode['Episode']['image'];
+		else
+			return $returnUrl . SERVER_PATH . IMAGE_PATH . $episode['Anime']['fanart'];
+
+	}
 }
 ?>
--- app/View/Episode/view.ctp
+++ app/View/Episode/view.ctp
@@ -73,11 +73,12 @@ if($isAdmin)
 </div>
 <div class="span3 pull-right">
 <?php
-if(file_exists(WWW_ROOT . EPISODE_IMAGE_PATH . $episode['Episode']['anime_id'] . "/" . $episode['Episode']['image']) && $episode['Episode']['image'] != null)
+echo '<img class="thumbnail" src="' . Episode::fetchImage($episode['Episode']['id'],219) . '">';
+/*if(file_exists(WWW_ROOT . EPISODE_IMAGE_PATH . $episode['Episode']['anime_id'] . "/" . $episode['Episode']['image']) && $episode['Episode']['image'] != null)
 	echo '<img class="thumbnail" src="http://src.sencha.io/219/'.SERVER_PATH . EPISODE_IMAGE_PATH . $episode['Episode']['anime_id'] . "/" . $episode['Episode']['image'].'">';
 else
 	echo '<img class="thumbnail" src="http://src.sencha.io/219/'.SERVER_PATH . IMAGE_PATH . $episode['Anime']['fanart'].'">';
-
+*/
 ?>
 <br>
 <table class="table table-striped table-condensed table-small">
