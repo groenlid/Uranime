db2d2396c95f3bdd65c25678c1567b5375c96783
Mats Groenlid <groenlid@gmail.com>
2012-06-28 01:52:02 +0200

    added timestamp to watchlist

--- app/Model/UserWatchlist.php
+++ app/Model/UserWatchlist.php
@@ -5,5 +5,9 @@ class UserWatchlist extends AppModel
 	//public $hasOne = 'User';
 	var $useTable = 'user_watchlist';
 	
+	function beforeSave($options) {
+		$this->data['UserWatchlist']['time'] = date("Y-m-d H:i:s");
+		return true;
+	}
 }
 ?>
